9ba4c265654ec336dd6f06323dd000e07bab95ba
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2023-08-14 07:07:15 -0700

    finished all the necessary functions for the app

--- app/features/game/gameSlice.js
+++ app/features/game/gameSlice.js
@@ -1,8 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { Alert } from 'react-native';
 
 const initialState = {
   screen: 'startGameScreen',
   pickedNumber: '',
+  computerNumber: 50,
+  computerMin: 1,
+  computerMax: 100,
+  lost: false,
+  numberOfGuesses: 0,
+  won: false,
 };
 
 const gameSlice = createSlice({
@@ -10,7 +17,7 @@ const gameSlice = createSlice({
   initialState,
   reducers: {
     changePage: (state, { payload }) => {
-      state.screen = payload;
+      state.screen = payload; // possible pages: startGameScreen, gameOverScreen, gameScreen
     },
     setEnteredNumber: (state, { payload }) => {
       const regex = /^[^A-Za-z\s,.\\-]*$/;
@@ -19,13 +26,67 @@ const gameSlice = createSlice({
           ? state.pickedNumber
           : payload;
     },
-    resetEnteredNumber: (state) => {
+    reset: (state) => {
       state.pickedNumber = '';
+      state.computerMax = 100;
+      state.computerMin = 1;
+      state.computerNumber = 50;
+      state.numberOfGuesses = 0;
+      state.lost = false;
+      state.won = false;
+    },
+    generateGuess: (state, { payload }) => {
+      const randomNum =
+        Math.floor(Math.random() * (state.computerMax - state.computerMin)) +
+        state.computerMin;
+      if (randomNum == payload) {
+        state.computerNumber =
+          Math.floor(Math.random() * (state.computerMax - state.computerMin)) +
+          state.computerMin;
+      } else {
+        state.computerNumber = randomNum;
+      }
+      state.numberOfGuesses++;
+      state.lost = false;
+    },
+    setComputerMin: (state) => {
+      if (state.computerNumber > state.pickedNumber) {
+        Alert.alert('Warning!!!', 'Liar,Liar Pants on Fire', [
+          { text: 'Sowwy!', style: 'cancel' },
+        ]);
+      } else {
+        state.computerMin = state.computerNumber + 1;
+      }
+    },
+    setComputerMax: (state) => {
+      if (state.computerNumber < state.pickedNumber) {
+        Alert.alert('Warning!!!', 'Liar,Liar Pants on Fire', [
+          { text: 'Sowwy!', style: 'cancel' },
+        ]);
+      } else {
+        state.computerMax = state.computerNumber;
+      }
+    },
+    checkComputersGuess: (state) => {
+      if (state.computerNumber == state.pickedNumber) {
+        state.lost = true;
+      }
+    },
+    youLost: (state) => {
+      state.screen = 'gameOverScreen';
     },
   },
 });
 
-export const { changePage, setEnteredNumber, resetEnteredNumber } =
-  gameSlice.actions;
+export const {
+  changePage,
+  setEnteredNumber,
+  reset,
+  generateGuess,
+  setComputerMax,
+  setComputerMin,
+  checkComputersGuess,
+  youLost,
+} = gameSlice.actions;
 
 export default gameSlice.reducer;
